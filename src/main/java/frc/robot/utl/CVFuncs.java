37d4956a7f7bd7615c2b63fb70397db5887b0373
Eric Wang <43788690+ericzw@users.noreply.github.com>
2020-01-21 16:51:05 -0500

    poseEstimation
    first attempt at using solvePnP

--- src/main/java/frc/robot/utl/CVFuncs.java
+++ src/main/java/frc/robot/utl/CVFuncs.java
@@ -6,15 +6,51 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.utl;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.*;
 /**
  * Add your docs here.
  */
 public class CVFuncs {
+	public static boolean estimatePose() {
+		List<Point3> objectPointsList = new ArrayList<Point3>();
+		objectPointsList.add(new Point3(-19.625, 0, 0));
+		objectPointsList.add(new Point3(19.625, 0, 0));
+		objectPointsList.add(new Point3(9.82, 17, 0));
+		objectPointsList.add(new Point3(-9.82, 17, 0));
+		
+		MatOfPoint3f objectPointsMat = new MatOfPoint3f();
+		objectPointsMat.fromList(objectPointsList);		
+		
+		List<Point> imagePointsList = new ArrayList<Point>();
+		imagePointsList.add(new Point(-19.625, 0));
+		imagePointsList.add(new Point(19.625, 0));
+		imagePointsList.add(new Point(9.82, 17));
+		imagePointsList.add(new Point(-9.82, 17));
+		
+		MatOfPoint2f imagePointsMat = new MatOfPoint2f();
+		imagePointsMat.fromList(imagePointsList);
+			
+		Mat cameraMatrix = new Mat();
+		MatOfDouble distCoeffs = new MatOfDouble();
+		Mat rvec = new Mat();
+		Mat tvec = new Mat();
+		
+		return (Calib3d.solvePnP(objectPointsMat, imagePointsMat, cameraMatrix, distCoeffs, rvec, tvec));
+	}
+	
     public double[] getXYZ(Mat a){
         return new double[0];
     }
-    public MatOfPoint2f data(){
-        Calib3d.solvePnP(objPointsMat, this, camMatrix, distCoeffs, Rvec, Tvec);
+    
+//    public MatOfPoint2f data(){
+//        Calib3d.solvePnP(objPointsMat, this, camMatrix, distCoeffs, Rvec, Tvec);
+//    }
+    
+    public static void main(String[] args) { // Test client
+    	estimatePose();
     }
 }
